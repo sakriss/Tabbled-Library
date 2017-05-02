c532a573dabc250ea744bfeae00da5550e68f1dd
Scott Kriss <skriss@deloitte.com>
2017-05-02 11:45:18 -0700

    Initial Commit

---
+++ Tabbed Library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tabbed Library
+//
+//  Created by Scott Kriss on 5/2/17.
+//  Copyright © 2017 Scott Kriss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
